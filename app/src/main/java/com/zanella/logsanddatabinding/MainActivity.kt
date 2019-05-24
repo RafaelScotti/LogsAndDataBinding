41175e5a3049a06113b05751795c5b9420957ba0
RafaelScotti <rafael.scotti@acad.pucrs.br>
2019-05-24 15:04:11 -0300

    .

--- app/src/main/java/com/zanella/logsanddatabinding/MainActivity.kt
+++ app/src/main/java/com/zanella/logsanddatabinding/MainActivity.kt
@@ -1,24 +1,15 @@
 package com.zanella.logsanddatabinding
 
-import android.databinding.BindingAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    var nome : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    @BindingAdapter("app:goneUnless")
-    fun goneUnless(view: View, visible: Boolean) {
-        view.visibility = if (visible) View.VISIBLE else View.GONE
-
-    }
-
-    fun btnDoSomething (view: View) {
-
-    }
+    
 }
