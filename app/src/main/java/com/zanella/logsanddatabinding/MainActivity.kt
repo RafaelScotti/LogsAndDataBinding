b2c45f9896975b53094f9a9e932b2d5d7e2cf4ff
RafaelScotti <rafael.scotti@acad.pucrs.br>
2019-05-24 16:47:21 -0300

    name, lastName, fullName()

--- app/src/main/java/com/zanella/logsanddatabinding/MainActivity.kt
+++ app/src/main/java/com/zanella/logsanddatabinding/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.zanella.logsanddatabinding
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.zanella.logsanddatabinding.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    var nome : String = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>( this, R.layout.activity_main )
+
+
+
+        val user = User( "Rafael", "Zanella" )
+
+        binding.user = user
     }
-    
+
+    fun goToUserActivity(view: View){
+        val intent = Intent(this, User::class.java)
+        startActivity(intent)
+    }
+
+
 }
