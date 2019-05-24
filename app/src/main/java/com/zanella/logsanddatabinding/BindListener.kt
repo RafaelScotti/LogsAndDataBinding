29019d09851f3a39662015a7ab75c4bfecb407b6
RafaelScotti <rafael.scotti@acad.pucrs.br>
2019-05-24 17:33:00 -0300

    Log

--- app/src/main/java/com/zanella/logsanddatabinding/BindListener.kt
+++ app/src/main/java/com/zanella/logsanddatabinding/BindListener.kt
@@ -1,10 +1,12 @@
 package com.zanella.logsanddatabinding
 
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 
 class BindListener {
     fun onClickButton(view: View, firstName:String, lastName: String) {
+        Log.i("BindListener", "button clicked!")
         Toast
             .makeText( view.context, "$firstName $lastName", Toast.LENGTH_SHORT )
             .show()
--- app/src/main/java/com/zanella/logsanddatabinding/MainActivity.kt
+++ app/src/main/java/com/zanella/logsanddatabinding/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zanella.logsanddatabinding
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zanella.logsanddatabinding.databinding.ActivityMainBinding
 
 
@@ -10,6 +11,7 @@ import com.zanella.logsanddatabinding.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("MainActivity", "On Create")
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil
@@ -19,7 +21,10 @@ class MainActivity : AppCompatActivity() {
         val user = User( "Rafael", "Zanella" )
 
         binding.user = user
+        Log.i("MainActivity", "data binding user fields")
         binding.bindListener = BindListener()
+
+        Log.i("MainActivity", "data binding click me btn")
 
     }
 
